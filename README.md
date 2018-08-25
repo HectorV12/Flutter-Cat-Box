bd234cc7153145e7d09a9d2000362a6377efdb74
HectorV12 <Hector_Vazquez@live.com>
2018-08-25 13:09:43 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# animation
+
+A Flutter Animation Example of a Cat in a Box.
+
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:animation/app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  //debugPaintSizeEnabled = true;
+  runApp(App());
+}
